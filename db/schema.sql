dc36133e9dc9a2acf272fc75f1aa9acf56a7b8e7
Chaltu Abdulkadir <chaltumabdulkadir@gmail.com>
2021-02-08 20:53:46 -0600

    added serveral updates to initial files

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS burger_db;
+USE burger_db;
+
+DROP TABLE IF EXISTS burgers;
+
+CREATE TABLE burgers (
+id INT AUTO_INCREMENT NOT NULL,
+burger_name VARCHAR(45) NOT NULL,
+devoured BOOLEAN DEFAULT false,
+PRIMARY KEY (id)
+);
+
